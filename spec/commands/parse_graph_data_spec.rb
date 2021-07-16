6a1aa3520890cbc1776519db6a1517f4bdb03af9
adolfrodeno <amvillalobos@uc.cl>
2021-07-15 21:05:20 -0400

    test(parseGraphData): additional test to parse input

--- spec/commands/parse_graph_data_spec.rb
+++ spec/commands/parse_graph_data_spec.rb
@@ -5,32 +5,50 @@ describe ParseGraphData do
     described_class.for(data: data)
   end
 
-  let(:valid_hash) do
+  let(:station1) do
     {
-      "metroStations" => [
-        {
-          "name": "Los heroes",
-          "neighbors": ["Toesca", "Cal y Canto"],
-          "color": 2
-        }
-      ]
+      "name": "Los heroes",
+      "neighbors": ["Toesca", "Cal y Canto"],
+      "color": 2
     }
   end
 
-  let(:output_hash) do
-      {
-       'Los heroes' =>  {
-          name: "Los heroes",
-          neighbors: ["Toesca", "Cal y Canto"],
-          color: 2
-        }
-      }
+  let(:station2) do
+    {
+      "name": "Toesca",
+      "neighbors": ["Los heroes", "Rondizzoni"],
+      "color": 1
+    }
   end
 
+  let(:data) { {"metroStations" => [ station1, station2]} }
+
+
   describe 'valid data' do
-    let(:data) { valid_hash}
-    it 'returns parsed hash' do
-      expect(perform).to eq(output_hash)
+    it 'parsed hash contain station names as keys' do
+      expect(perform).to have_key("Toesca")
+      expect(perform).to have_key("Los heroes")
+    end
+
+    it 'parsed stations contain symbolized names' do
+      expect(perform).to include(
+        "Los heroes" => hash_including(name: "Los heroes"),
+        "Toesca" => hash_including(name: "Toesca")
+      )
+    end
+
+    it 'parsed stations contain symbolized colors' do
+      expect(perform).to include(
+        "Los heroes" => hash_including(color: 2),
+        "Toesca" => hash_including(color: 1)
+      )
+    end
+
+    it 'parsed stations contain symbolized neighbor' do
+      expect(perform).to include(
+        "Los heroes" => hash_including(neighbors:  station1[:neighbors]),
+        "Toesca" => hash_including(neighbors: station2[:neighbors])
+      )
     end
   end
 end
