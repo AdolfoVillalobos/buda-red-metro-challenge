f79cbb0a4c0ee13ed8bcffc1f0c5129109870030
adolfrodeno <amvillalobos@uc.cl>
2021-07-16 08:22:17 -0400

    test(station): add tests for associations, validations and methods

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,5 +1,6 @@
 class Station < ApplicationRecord
   belongs_to :metro_network
+  has_many :sink_edges, :class_name => 'Edge', :foreign_key => 'source_id'
 
   enum color: { no_color: 0, green: 1, red: 2}
   enum status: { no_visited: 0, visited: 1, done: 2}
@@ -9,4 +10,16 @@ class Station < ApplicationRecord
                 inclusion: { in: Station.colors.keys }
   validates :status, presence: true,
                 inclusion: { in: Station.statuses.keys }
+
+  def reset
+    update(status: 0, distance_from_source: 2**32)
+  end
+
+  def reset_source
+    update(distance_from_source: 0)
+  end
+
+  def visit(new_distance)
+    update(status: 1, distance_from_source: new_distance)
+  end
 end
