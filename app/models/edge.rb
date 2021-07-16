e01dc9da3918e6e5b15376323eb8d9d8707576f1
adolfrodeno <amvillalobos@uc.cl>
2021-07-16 16:39:02 -0400

    test(edge): add Edge tests for updating cost

--- app/models/edge.rb
+++ app/models/edge.rb
@@ -1,4 +1,13 @@
 class Edge < ApplicationRecord
   belongs_to :source, :class_name => 'Station', :foreign_key => 'source_id'
   belongs_to :target, :class_name => 'Station', :foreign_key => 'target_id'
+
+  def new_cost(train_color)
+    return 1 if train_color == 0
+
+    return 1 if source.no_color?
+    return 1 if source.color == target.color
+    return 1 if target.no_color?
+    return 0
+  end
 end
